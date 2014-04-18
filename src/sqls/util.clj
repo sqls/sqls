9c6e185e7fc56bbe353ef40082695eb7cda7d55e
Maciej Pietrzak <maciej@hell.cx>
2014-04-18 12:46:00 +0200

    Fix type error, use Strings where possible instead of (platform specific) java.io.File

--- src/sqls/util.clj
+++ src/sqls/util.clj
@@ -55,7 +55,7 @@
   [d]
   (let [^java.io.File df (if (instance? java.io.File d) d (java.io.File. d))
         filenames (seq (.list df))
-        join-df-and-filename (fn [f] (path-join [df f]))
+        join-df-and-filename (fn [^String f] (path-join [(.getAbsolutePath df) f]))
         absolute-filenames (map join-df-and-filename filenames)]
     absolute-filenames))
 
@@ -64,14 +64,11 @@
   "Return a list of absolute paths to driver jar files."
   []
   (let [home (java.lang.System/getProperty "user.home")
-        _ (println "home:" home)
         home-sqls (path-join [home ".sqls"])
         home-sqls-lib (path-join [home ".sqls" "lib"])
         dirs ["." "lib" home-sqls home-sqls-lib]
-        _ (println "dirs to search for jar files:" dirs)
         is-jar? (fn [s] (endswith s ".jar"))
         list-jars (fn [d] (filter is-jar? (list-dir d)))
         lists-of-jars (map list-jars dirs)
         jars (apply concat lists-of-jars)]
-    (println "jar files:" jars)
     jars))
