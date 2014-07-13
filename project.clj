162119079f52dbacfee6f7c5e8db524d7b14c90a
Maciej Pietrzak <maciej@hell.cx>
2014-07-13 02:54:53 +0200

    Update dependencies and move gc logging options to dev profile

--- project.clj
+++ project.clj
@@ -5,19 +5,18 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.4"]
-                 [org.clojure/java.jdbc "0.3.3"]
-                 [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/java.jdbc "0.3.4"]
+                 [org.clojure/tools.logging "0.3.0"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [seesaw "1.4.4"]
                  ]
   :main sqls.core
   :java-source-paths ["src"]
   :target-path "target/%s"
-  :plugins [[codox "0.6.7"]
+  :plugins [[codox "0.8.10"]
             [lein-ancient "0.5.5"]]
   :codox {:output-dir "doc/codox"}
-  :profiles {:uberjar {:aot :all}}
-  :jvm-opts ["-Xms4M" "-Xmx1G" "-XX:-PrintGC"])
   :profiles {:uberjar {:aot :all}
-             :dev {:global-vars {*warn-on-reflection* true}}}
+             :dev {:global-vars {*warn-on-reflection* true}
+                   :jvm-opts ["-Xms4M" "-Xmx1G" "-XX:+PrintGC" "-XX:+UseG1GC"]}})
