e4437cc885861903fa29d2e6830e7bea0f71305d
Maciej Pietrzak <maciej@hell.cx>
2015-05-27 20:09:21 +0200

    Bump some deps, including Clojure.

--- project.clj
+++ project.clj
@@ -3,19 +3,18 @@
   :url "https://github.com/mpietrzak/sqls"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.clojure/java.jdbc "0.3.6"]
+                 [org.clojure/java.jdbc "0.3.7"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.xerial/sqlite-jdbc "3.8.7"]
+                 [org.xerial/sqlite-jdbc "3.8.10.1"]
                  ;[com.taoensso/timbre "3.4.0"]
-                 [seesaw "1.4.5"]
-                 ]
+                 [seesaw "1.4.5"]]
   :main sqls.core
   :java-source-paths ["src"]
   :target-path "target/%s"
-  :plugins [[codox "0.8.11"]]
+  :plugins [[codox "0.8.12"]]
   :codox {:output-dir "doc/codox"}
   :profiles {:uberjar {:aot :all}
              :dev {:global-vars {*warn-on-reflection* true}
